b713c5ec8bafe726ae4475e01b6843605efc294a
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-03-18 19:08:58 +0530

    RTK-Qary started

---
+++ RTK-quary/src/redux/api.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const posts = createApi({
+     reducerPath: "api",
+     baseQuery: fetchBaseQuery({
+          baseUrl: 'http://localhost:3000/'
+     }),
+     endpoints: (builder)=>({
+          getPosts: builder.query<string, string>({ query: ()=> "posts" }),
+     })
+})
+
+export const { useGetPostsQuery } = posts;
