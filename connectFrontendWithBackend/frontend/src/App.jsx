71bd2cc1ad52a5dddbc943da3ca5890fcdd4a345
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-05-01 20:09:29 +0530

    done

--- connectFrontendWithBackend/frontend/src/App.jsx
+++ connectFrontendWithBackend/frontend/src/App.jsx
@@ -1,9 +1,33 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const getJokes = async()=> {
+    try {
+      const res = await axios.get('/api/jokes');
+      setJokes(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+    getJokes();
+  },[])
+  const [jokes, setJokes] = useState([]);
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Chai and react</h1>
+      <p>JOKES: {jokes.length}</p>
+      {jokes && jokes.map((joke, index) => (
+        <>
+          <h3 key={joke.id}>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </>
+      ))} 
+    </div>
+  );
 }
 
-export default App
+export default App;
